164733fe3f55dba7c4e28ab581353fb7c2faf844
Umang Tiwari <umangtiwari52@gmail.com>
2024-08-18 20:01:20 +0530

    comparing two card choices

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import SingleCard from './SingleCard'
 //first we will need an array of cards or you can say there image sources there are 6 of them
@@ -16,6 +16,11 @@ const arr =[
 function App() {
   const [cards,setCards] = useState([])//initially we keep an empty array
   const [turns,setTurns]= useState(0)//we also need to count states
+  const [firstChoice,setFirstChoice]= useState(null)//this will be updated when the cards are clicked on the back
+  const [secondChoice,setSecondChoice]=useState(null)
+  const handleChoice =(card)=>{
+    firstChoice?setSecondChoice(card):setFirstChoice(card)
+  }
   //then we need a function which does 3 things for us, first is that we need 2 of each of these ards in our array , then we need to shuffle the arrays,
 //and provide unique ids to each of these arrays.
 const shuffleCards=()=>{
@@ -27,9 +32,32 @@ const shuffleCards=()=>{
   setCards(shuffledCards)
   setTurns(0)
   //we ll call this entire function when new game is clicked
+  
 
+  
+}
 
-  console.log(cards,turns)
+//now this to check if the selected cards are same or not for that we will use useEffect hook 
+//we use use effect because handle choice function will take its own time to change the state as state updates are scheduled
+// so we use useEffect so that we dont check before the state update happens
+useEffect(()=>{
+  if(firstChoice && secondChoice){//if both choices are there only then we compare or else no point comparing
+    if(firstChoice.src === secondChoice.src){
+      console.log("they are the same")
+      resetTurn()
+    }
+    else{
+      console.log("they are different")
+      resetTurn()
+    }
+  }
+},[firstChoice,secondChoice])
+
+//this function is to rest the choices after two cards have been selected and to increment the number of turns
+const resetTurn = ()=>{
+  setFirstChoice(null)
+  setSecondChoice(null)
+  setTurns(prevTurns => prevTurns+1)
 }
   return (
     //here on the 5th line we added the card grid make a div of card grid class so we can use css later
@@ -39,13 +67,18 @@ const shuffleCards=()=>{
 
 
     //go to singlecard.js for next revision
+
+    //we add new prop called handlechoice and pass it to single card as prop as this function can only change the state present in this file
+    //named choice one and choice two 
+    // so to change these states from another component we will have to pass the functions which change the state in this component  into that component
+    // so over there we can change the state of what is here. 
     <div className="App">
       <h1>Magic Match</h1>
       <button onClick={shuffleCards}>New Game</button>
       
       <div className='card-grid'>
         {cards.map(card=>(
-          <SingleCard key={card.id} card={card}/>//here we will have to pass card property so in singlecard.js it can access src
+          <SingleCard key={card.id} card={card} handleChoice ={handleChoice}/>//here we will have to pass card property so in singlecard.js it can access src
         ))
 
         }
--- src/SingleCard.js
+++ src/SingleCard.js
@@ -1,10 +1,13 @@
 import './SingleCard.css'
 //here we make a component instead of mapping it all together over there we take card as parameter so we can access the src 
-export default function SingleCard({card}) {
+export default function SingleCard({card,handleChoice}) {
+    const handleClick=()=>{
+        handleChoice(card)
+    }
   return (
     <div className='card'>
             <img class="front" src={card.src} alt='front'></img>
-            <img class="back" src="/img/cover.png" alt='back'></img>
+            <img class="back" src="/img/cover.png" alt='back' onClick={handleClick}></img>
     </div>
   )
 }
